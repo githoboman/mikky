4939b3c0e07cd817032b99608aceee5905159099
githoboman <smithmar681@gmail.com>
2024-10-18 10:33:04 +0100

    pics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ const Loader = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gradient-to-r from-gray-800 to-cyan-400">
       <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
-      
     </div>
   )
 }
